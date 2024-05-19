2be39dea34eb9d2f42136213047a4f0579d53a5a
YoHaiYo <12si47bun@naver.com>
2024-05-19 17:21:53 +0900

    기본 CRUD 구현

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import { supabase } from './config/dbconfig/supabaseClient.js'
 
 const countries = ref([])
+const newName = ref('')
 
 async function getCountries() {
   const { data } = await supabase.from('countries').select()
   countries.value = data
 }
 
-onMounted(() => {
-  getCountries()
-})
+async function addCountry() {
+  if (newName.value.trim() !== '') {
+    const { data, error } = await supabase.from('countries').insert([{ name: newName.value }])
+    if (error) {
+      console.error('Error adding country:', error.message)
+    } else {
+      if (data && data.length > 0) {
+        countries.value.push(data[0])
+        newName.value = ''
+        await getCountries() // 추가된 데이터를 다시 가져옴
+      }
+    }
+  }
+  // 화면바로반영안됨. 임시로 새로고침으로.
+  window.location.reload()
+}
+
+async function updateCountry(id, newName) {
+  const { data, error } = await supabase.from('countries').update({ name: newName }).eq('id', id)
+  if (error) {
+    console.error('Error updating country:', error.message)
+  } else {
+    const index = countries.value.findIndex(country => country.id === id)
+    if (index !== -1) {
+      countries.value[index].name = newName
+    }
+  }
+}
+
+async function deleteCountry(id) {
+  const { error } = await supabase.from('countries').delete().eq('id', id)
+  if (error) {
+    console.error('Error deleting country:', error.message)
+  } else {
+    countries.value = countries.value.filter(country => country.id !== id)
+  }
+}
+
+getCountries() // 컴포넌트가 마운트될 때 데이터를 가져옴
 </script>
 
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
+  <h1>LinkHub</h1>
+  <div>
+    <ul>
+      <li v-for="country in countries" :key="country.id">
+        <input type="text" v-model="country.name" @blur="updateCountry(country.id, country.name)">
+        <button @click="deleteCountry(country.id)">Delete</button>
+      </li>
+    </ul>
+    <input type="text" v-model="newName">
+    <button @click="addCountry">Add Country</button>
+  </div>
 </template>
